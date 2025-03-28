05941fbf29054b6415ed1804618c4b2cf81bb85e
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-03-28 22:00:10 +0600

    complete project

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
-import './style.css';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper(".mySwiper", {
+   modules: [Navigation, Pagination],
+   loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
